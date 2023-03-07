a01d3ead90dabc6612c521dbaaa54e88e9dee835
Marcos <marcosws@ymail.com>
2023-03-07 10:47:13 -0300

    inicio do projeto

---
+++ src/main/java/com/github/marcosws/vehicle/ApiRestVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcosws.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestVehicleApplication.class, args);
+	}
+
+}
